a93af84b85428c6a5cb4df4fd6005b152adcba1c
Cameron Abel <cameronabel@gmail.com>
2022-10-26 21:46:48 -0700

    added basic conversion functions and buttons

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,38 @@ function convertPoundtoOunce (poundNum) {
   return (poundNum * 16);
 }
 
+function convertOuncetoPound (ounceNum) {
+  return (ounceNum / 16);
+}
+
 function convertOunceToGram(ounceNum) {
   return (ounceNum * 28.3495);
 }
 
-//interface logic
+function convertGramToOunce(gramNum) {
+  return (gramNum / 28.3495);
+}
 
-let poundNum = parseInt(window.prompt("Enter how many pounds:"));
+function executeConversionToGram() {
+  const pounds = parseFloat(document.getElementById("pound-field").value);
+  let ounces = parseFloat(document.getElementById("ounce-field").value);
+  ounces += convertPoundtoOunce(pounds);
+  const grams = convertOunceToGram(ounces);
+  document.getElementById("gram-field").value = grams.toFixed(3);
+}
 
-let ounceNum = parseInt(window.prompt("Enter how many ounces:"));
+function executeConversionToPound() {
+  const grams = parseFloat(document.getElementById("gram-field").value);
+  let ounces = convertGramToOunce(grams);
+  let pounds = convertOuncetoPound(ounces);
+  [pounds, ounces] = pounds.toString().split('.');
+  ounces = convertPoundtoOunce(parseFloat("0." + ounces));
+  document.getElementById("pound-field").value = pounds;
+  document.getElementById("ounce-field").value = ounces.toFixed(3);
+}
+
+function clearAll() {
+  document.getElementById("pound-field").value = '';
+  document.getElementById("ounce-field").value = '';
+  document.getElementById("gram-field").value = '';
+}
